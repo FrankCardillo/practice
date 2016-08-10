5ccd170c4b4b05ae89ad3c2b475962b46f028f1c
Frank Cardillo <facardillo91@gmail.com>
2016-08-09 20:40:28 -0400

    Python #3

---
+++ codewars/python_practice/credit_card_mask.py
@@ -0,0 +1,14 @@
+# Usually when you buy something, you're asked whether your credit card number,
+# phone number or answer to your most secret question is still correct.
+# However, since someone could look over your shoulder, you don't want
+# that shown on your screen. Instead, we mask it.
+#
+# Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+def maskify(cc):
+    if len(cc) <= 4:
+        return ''
+    else:
+        length_minus_last_four = len(cc)-4
+        last_four = cc[-4:]
+        return '#' * length_minus_last_four + last_four
