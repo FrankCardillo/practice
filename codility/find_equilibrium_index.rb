7bcecb436a9f175d26a8ce3b411cf076c43e827c
Frank Cardillo <facardillo91@gmail.com>
2016-10-19 12:59:07 -0400

    find equilibrium index in array

---
+++ codility/find_equilibrium_index.rb
@@ -0,0 +1,18 @@
+def solution(a)
+  if a.length <= 1
+    return -1
+  end
+
+  a.each_with_index do |element, index|
+    if index == a.length-1
+      if a[0..index].reduce(:+) == 0
+        return index
+      end
+    end
+    if a[0..index].reduce(:+) == a[index..-1].reduce(:+)
+      return index
+    end
+  end
+  
+  return -1
+end
